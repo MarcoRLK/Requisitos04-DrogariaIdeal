851480502864d0959ff3fb5646adba1ddd224e0b
Marco Kuroiva <marco.antoniorl10@gmail.com>
2017-11-20 16:53:50 -0200

    Setting up tests and testing home page render

---
+++ DrogariaIdeal/test_settings.py
@@ -0,0 +1,9 @@
+from DrogariaIdeal.settings import *
+# this import is actually necessary
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
