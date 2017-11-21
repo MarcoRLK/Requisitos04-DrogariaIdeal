e1957ab5f362d9d1da5ea9c1a8be24a55d1b79a3
andersonms1 <andersoonn.ams@gmail.com>
2017-11-20 22:09:11 -0200

    Adding static file constant

--- DrogariaIdeal/settings_heroku.py
+++ DrogariaIdeal/settings_heroku.py
@@ -98,6 +98,13 @@ DATABASES = {
     }
 }
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
